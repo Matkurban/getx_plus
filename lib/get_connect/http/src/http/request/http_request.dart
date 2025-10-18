4ae4bd064ac9054ede7df43901e4afb577dbda68
matkurban <3496354336@qq.com>
2025-10-18 12:59:29 +0800

    add example test

--- lib/get_connect/http/src/http/request/http_request.dart
+++ lib/get_connect/http/src/http/request/http_request.dart
@@ -1,7 +1,7 @@
 import 'package:getx_plus/get_connect/http/src/certificates/certificates.dart';
 import 'package:getx_plus/get_connect/http/src/http/stub/http_request_stub.dart'
     if (dart.library.js_interop) 'package:getx_plus/get_connect/http/src/http/html/http_request_html.dart'
-    if (dart.library.io) 'package:getx_plus//get_connect/http/src/http/io/http_request_io.dart';
+    if (dart.library.io) 'package:getx_plus/get_connect/http/src/http/io/http_request_io.dart';
 
 HttpRequestImpl createHttp({
   bool allowAutoSignedCert = true,
